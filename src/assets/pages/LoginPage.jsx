d0d0ccc26e8dc683a60607f15af9e28efde68c6e
unknown <minhtuchau2@gmail.com>
2024-01-20 17:53:52 -0500

    made-file

--- src/assets/pages/LoginPage.jsx
+++ src/assets/pages/LoginPage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
-
+import { useNavigate } from "react-router-dom";
 
 function LoginPage({ setIsAuth }){
+
+let navigate = useNavigate();
+
 const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
       setIsAuth(true);
-      navigate("/");
+      navigate("/GamePage");
     });
   };
 
