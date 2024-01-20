3d555de392805dd3148af25b2e7271602c64100f
unknown <minhtuchau2@gmail.com>
2024-01-20 17:39:08 -0500

    made-file

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./assets/pages/HomePage";
 import LoginPage from "./assets/pages/LoginPage";
 import GamePage from "./assets/pages/GamePage";
+import { useState } from "react";
 
 function App(){
+ const [isAuth, setIsAuth] = useState(false);
 
 return (
 
@@ -24,8 +26,9 @@ return (
 
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                 <Route path="/loginpage" element={<LoginPage />} />
-                  <Route path="/GamePage" element={<GamePage />} />
+                <Route path="/loginpage" element={<LoginPage setIsAuth={setIsAuth} />} />
+                <Route path="/GamePage" element={<GamePage />} />
+
             </Routes>
         </Router>
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import the functions you need from the SDKs you need
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +26,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
